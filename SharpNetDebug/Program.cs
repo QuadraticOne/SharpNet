2bee4117591fa7466389e94e90b00f6cf32f09d6
aatack <alexanderjtatack@gmail.com>
2017-09-05 22:28:05 +0100

    Add project files.

---
+++ SharpNetDebug/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SharpNetDebug
+{
+
+    /// <summary>
+    /// Program for debugging SharpNet.  Does not contain any useful code.
+    /// </summary>
+    public class Program
+    {
+
+        /// <summary>
+        /// Debugs SharpNet.  Code added here will be run when the project is run.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Debugging SharpNet.");
+
+            #region DEBUG_CODE
+
+            #endregion
+
+            Console.WriteLine("Debugging has finished.  Press ENTER to exit.");
+            Console.Read();
+
+        }
+
+    }
+
+}
