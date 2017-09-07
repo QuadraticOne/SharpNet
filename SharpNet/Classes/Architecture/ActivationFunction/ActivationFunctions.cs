2a62cad306d58077b3be47eee0786890d4a28b0d
aatack <alexanderjtatack@gmail.com>
2017-09-07 22:25:23 +0100

    Add container class for common activation functions Implement sigmoid activation function

---
+++ SharpNet/Classes/Architecture/ActivationFunction/ActivationFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Architecture.ActivationFunction
+{
+
+    /// <summary>
+    /// Contains some commonly used activation functions.
+    /// </summary>
+    public static class ActivationFunctions
+    {
+
+        /// <summary>
+        /// Maps inputs to the range (0, 1).  Approximately linear at the origin.
+        /// </summary>
+        public class Sigmoid : IActivationFunction
+        {
+
+            public double Value(double x)
+            {
+                return 1 / (1 + Math.Exp(-x));
+            }
+
+            public double Derivative(double x)
+            {
+                return Value(x) * (1 - Value(x));
+            }
+
+        }
+
+    }
+
+}
