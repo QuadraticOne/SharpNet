e16a25d66e8598801dae181c2b7059964bfe43cf
aatack <alexanderjtatack@gmail.com>
2017-09-07 22:24:38 +0100

    Add layer abstract class Add feedforward layer class

---
+++ SharpNet/Classes/Architecture/Layer/Layer.cs
@@ -0,0 +1,80 @@
+﻿using SharpNet.Classes.Maths;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Architecture.Layer.Layers
+{
+
+    /// <summary>
+    /// Parent class which defines behaviour for neural network layers.  Specifically, a layer
+    /// which inherits from this class takes a matrix of inputs with n rows and, by performing some
+    /// process, produces an output matrix with m rows.
+    /// </summary>
+    public abstract class Layer
+    {
+
+        public int Inputs { get; protected set; }
+        public int Outputs { get; protected set; }
+
+        protected Matrix _input;
+        public virtual Matrix Input {
+            get
+            {
+                return _input;
+            }
+
+            set
+            {
+                _input = ProcessInput(value);
+                outputIsAccurate = false;
+            }
+        }
+
+        protected Matrix _output;
+        public virtual Matrix Output
+        {
+            get
+            {
+                if (!outputIsAccurate)
+                {
+                    UpdateOutput();
+                    outputIsAccurate = true;
+                }
+                return _output;
+            }
+
+            protected set
+            {
+                _output = value;
+            }
+        }
+
+        protected bool outputIsAccurate;
+
+        /// <summary>
+        /// Give a new input to the layer, calculate the output, and return it.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public Matrix GetOutput(Matrix input)
+        {
+            Input = input;
+            return this.Output;
+        }
+
+        /// <summary>
+        /// Take an input matrix, check that it is a valid input matrix, perform any processing
+        /// necessary to turn it into a valid matrix, and then return it.
+        /// </summary>
+        /// <param name="matrix"></param>
+        protected abstract Matrix ProcessInput(Matrix matrix);
+
+        /// <summary>
+        /// Re-calculate the output vector and update the output field accordingly.
+        /// </summary>
+        protected abstract void UpdateOutput();
+
+    }
+
+}
---
+++ SharpNet/Classes/Architecture/Layer/Layers/FeedForwardLayer.cs
@@ -0,0 +1,101 @@
+﻿using SharpNet.Classes.Architecture.ActivationFunction;
+using SharpNet.Classes.Maths;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Architecture.Layer.Layers
+{
+
+    /// <summary>
+    /// Defines a fully connected feedforward of a multilayer network.
+    /// </summary>
+    public class FeedForwardLayer : Layer
+    {
+
+        private IActivationFunction _activation;
+        public IActivationFunction Activation
+        {
+            get { return _activation; }
+
+            set
+            {
+                outputIsAccurate = false;
+                _activation = value;
+            }
+        }
+
+        private Matrix _preActivation;
+        public Matrix PreActivation
+        {
+            get
+            {
+                if (!outputIsAccurate) UpdateOutput();
+                return _preActivation;
+            }
+
+            protected set
+            {
+                outputIsAccurate = false;
+                _preActivation = value;
+            }
+        }
+
+        // In the weight matrix, rows are outputs and columns are inputs
+        private Matrix _weights;
+        public Matrix Weights
+        {
+            get { return _weights; }
+
+            set
+            {
+                _weights = value;
+                outputIsAccurate = false;
+            }
+        }
+
+        /// <summary>
+        /// Define a new fully connected feedforward layer.  The number of inputs does not include
+        /// a bias term; this is added automatically.
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <param name="outputs"></param>
+        public FeedForwardLayer(int inputs, int outputs)
+        {
+            Inputs = inputs;
+            Outputs = outputs;
+
+            Input = new Matrix(Inputs, 1);
+            Output = new Matrix(Outputs, 1);
+
+            Weights = new Matrix(Outputs, Inputs + 1);
+        }
+
+        /// <summary>
+        /// Updates the output.
+        /// </summary>
+        protected override void UpdateOutput()
+        {
+            _preActivation = Weights * Input;
+            _output = _preActivation.ApplyPiecewiseFunction(Activation.Value);
+            outputIsAccurate = true;
+        }
+
+        /// <summary>
+        /// Processes the given raw input into a form suitable for the layer to take directly.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        protected override Matrix ProcessInput(Matrix matrix)
+        {
+            if ((matrix.Rows != Inputs) || (matrix.Columns != 1)) throw new ArgumentException(
+                "The input matrix is not of the correct dimension.");
+            Matrix newMatrix = new Matrix(Inputs + 1, 1);
+            newMatrix[0, 0] = 1;
+            for (int i = 0; i < matrix.Rows; i++) newMatrix[i + 1, 0] = matrix[i, 0];
+            return newMatrix;
+        }
+
+    }
+
+}
