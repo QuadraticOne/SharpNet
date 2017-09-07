1ec01db75779b6426bac6f003f9e5b230465829b
aatack <alexanderjtatack@gmail.com>
2017-09-07 22:25:49 +0100

    Add spare feedforward layer

---
+++ SharpNet/Classes/Architecture/Layer/Layers/SparseFeedForwardLayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Architecture.Layer.Layers
+{
+    public class SparseFeedForwardLayer
+    {
+        // NYI
+    }
+}
