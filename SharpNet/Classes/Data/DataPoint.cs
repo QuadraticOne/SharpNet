c255e4d9efc045eb326e0793d8a40aa60398164c
aatack <alexanderjtatack@gmail.com>
2017-09-08 18:09:33 +0100

    Add data point and data set classes

---
+++ SharpNet/Classes/Data/DataPoint.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Data
+{
+
+    /// <summary>
+    /// Stores supervised or unsupervised training examples for regression, classification, or
+    /// unsupervised learning.
+    /// </summary>
+    public struct DataPoint
+    {
+
+        public double[] input;
+        public int? category;
+        public double[] output;
+
+        /// <summary>
+        /// Constructor for regression training examples.
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <param name="outputs"></param>
+        public DataPoint(double[] input, double[] output)
+        {
+            this.input = input;
+            this.output = output;
+            category = null;
+        }
+
+        /// <summary>
+        /// Constructor for classification training examples.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="category"></param>
+        public DataPoint(double[] input, int category)
+        {
+            this.input = input;
+            output = null;
+            this.category = category;
+        }
+
+        /// <summary>
+        /// Constructor for unsupervised training examples.
+        /// </summary>
+        /// <param name="input"></param>
+        public DataPoint(double[] input)
+        {
+            this.input = input;
+            output = null;
+            category = null;
+        }
+
+    }
+
+}
---
+++ SharpNet/Classes/Data/DataSet.cs
@@ -0,0 +1,19 @@
+﻿using SharpNet.Classes.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Data
+{
+
+    /// <summary>
+    /// Abstract data set class from which other kinds of data sets are derived.
+    /// </summary>
+    public abstract class DataSet
+    {
+        
+
+
+    }
+
+}
