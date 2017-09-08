58ae850c8ed9ac35469864ca55fbf75c95ba0d82
aatack <alexanderjtatack@gmail.com>
2017-09-08 20:41:57 +0100

    Add outline for class loader

---
+++ SharpNet/Classes/Utils/ClassLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Utils
+{
+
+    /// <summary>
+    /// Loads and saves classes.
+    /// </summary>
+    public static class ClassLoader
+    {
+
+        /// <summary>
+        /// Take the string representation of an array of doubles which contains information about
+        /// an instance of a class, and from that built the instance.  Then return the instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        private static T StringToObject<T>(string inputString) where T : IClassUtils<T>, new()
+        {
+            T newObject = new T();
+
+            string[] stringArray = inputString.Split(new char[] { ',' });
+            double[] doubleArray = new double[stringArray.Length];
+
+            newObject.FromArray(doubleArray);
+            return newObject;
+        }
+
+    }
+
+}
