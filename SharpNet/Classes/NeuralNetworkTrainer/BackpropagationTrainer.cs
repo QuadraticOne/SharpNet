ea46f3214d63f574f850647e9b0c8872074d8772
aatack <alexanderjtatack@gmail.com>
2017-09-08 22:51:20 +0100

    Add backpropagation trainer

---
+++ SharpNet/Classes/NeuralNetworkTrainer/BackpropagationTrainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.NeuralNetworkTrainer
+{
+
+    /// <summary>
+    /// Class which trains feedforward networks on data for regression or classification problems.
+    /// </summary>
+    public class BackpropagationTrainer
+    {
+
+
+
+    }
+
+}
