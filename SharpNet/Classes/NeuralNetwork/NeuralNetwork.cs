3b7036f5e9a0f0e2b5f6a953cd9849d216ded351
aatack <alexanderjtatack@gmail.com>
2017-09-08 16:04:20 +0100

    Add neural network abstract class and feedforward net

---
+++ SharpNet/Classes/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,65 @@
+﻿using SharpNet.Classes.Maths;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.NeuralNetwork
+{
+
+    /// <summary>
+    /// Base class from which all neural networks inherit.  The primary function of a neural
+    /// network is to take an input vector, and convert it to an output vector by some transform
+    /// function.
+    /// </summary>
+    public abstract class NeuralNetwork
+    {
+
+        public int Inputs { get; protected set; }
+        public int Outputs { get; protected set; }
+
+        protected Matrix _input;
+        public Matrix Input
+        {
+            get { return _input; }
+
+            set
+            {
+                if ((value.Rows != Inputs) || (value.Columns != 1)) throw new ArgumentException(
+                    "The structure of this input is not compatible with the network.");
+
+                _input = value;
+                UpdateOutput();  // Update output immediately, for backpropagation
+            }
+        }
+
+        protected Matrix _output;
+        public Matrix Output
+        {
+            get
+            {
+                if (_output == null) UpdateOutput();
+                return _output;
+            }
+
+            protected set { _output = value; }
+        }
+
+        /// <summary>
+        /// Get the output corresponding to a given input.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public Matrix GetOutput(Matrix input)
+        {
+            Input = input;
+            return Output;
+        }
+
+        /// <summary>
+        /// Update the output of the network so that it is valid, given the input.
+        /// </summary>
+        protected abstract void UpdateOutput();
+
+    }
+
+}
---
+++ SharpNet/Classes/NeuralNetwork/NeuralNetworks/FeedForwardNetwork.cs
@@ -0,0 +1,98 @@
+﻿using SharpNet.Classes.Architecture.ActivationFunction;
+using SharpNet.Classes.Architecture.Layer.Layers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.NeuralNetwork.NeuralNetworks
+{
+
+    /// <summary>
+    /// A feedforward network which deterministically and independently produces an output vector
+    /// when given an input vector.
+    /// </summary>
+    public class FeedForwardNetwork : NeuralNetwork
+    {
+
+        private List<FeedForwardLayer> layers = new List<FeedForwardLayer>();
+
+        /// <summary>
+        /// Set up a feedforward network with the number of inputs and outputs.  Then add layers to
+        /// it using AddHiddenLayer() and AddOutputLayer().
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <param name="outputs"></param>
+        public FeedForwardNetwork(int inputs, int outputs)
+        {
+            Inputs = inputs;
+            Outputs = outputs;
+        }
+
+        /// <summary>
+        /// Add a hidden layer to the network, then returns the network.
+        /// </summary>
+        /// <param name="nodes"></param>
+        /// <param name="activation"></param>
+        /// <returns></returns>
+        public FeedForwardNetwork AddHiddenLayer(int nodes, IActivationFunction activation)
+        {
+            layers.Add(new FeedForwardLayer(
+                (layers.Count == 0 ? Inputs : layers[layers.Count - 1].Outputs), nodes)
+            {
+                Activation = activation
+            });
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds an output layer to the network, then returns the network.
+        /// </summary>
+        /// <param name="activation"></param>
+        /// <returns></returns>
+        public FeedForwardNetwork AddOutputLayer(IActivationFunction activation)
+        {
+            layers.Add(new FeedForwardLayer(layers[layers.Count - 1].Outputs,
+                Outputs)
+            {
+                Activation = activation
+            });
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds multiple hidden layers, each with the same number of nodes, to the network, then
+        /// returns the network.
+        /// </summary>
+        /// <param name="layers"></param>
+        /// <param name="nodesPerLayer"></param>
+        /// <param name="activation"></param>
+        /// <returns></returns>
+        public FeedForwardNetwork AddMultipleLayers(int layers, int nodesPerLayer,
+            IActivationFunction activation)
+        {
+            for (int i = 0; i < layers; i++)
+            {
+                AddHiddenLayer(nodesPerLayer, activation);
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Update the output of the network so that it is consistent with the input.
+        /// </summary>
+        protected override void UpdateOutput()
+        {
+            layers[0].Input = Input;
+            for (int i = 1; i < layers.Count; i++)
+            {
+                layers[i].Input = layers[i - 1].Output;
+            }
+            Output = layers[layers.Count - 1].Output;
+        }
+
+    }
+
+}
