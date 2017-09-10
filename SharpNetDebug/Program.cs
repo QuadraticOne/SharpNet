08bc26b0da35b67b753ebddb8bf7b1764c6b0ce6
aatack <alexanderjtatack@gmail.com>
2017-09-10 23:32:39 +0100

    Add unit tests

--- SharpNetDebug/Program.cs
+++ SharpNetDebug/Program.cs
@@ -30,42 +30,48 @@ namespace SharpNetDebug
             #region DEBUG_CODE
 
             // Create data set
-            DataSet.Regression ds = new DataSet.Regression(2, 1);  // XOR
-            ds.AddDataPoint(new double[] { 0, 0 }, new double[] { 0 });
-            ds.AddDataPoint(new double[] { 1, 0 }, new double[] { 1 });
-            ds.AddDataPoint(new double[] { 0, 1 }, new double[] { 1 });
-            ds.AddDataPoint(new double[] { 1, 1 }, new double[] { 0 });
-            Random rand = new Random();
-            /*for (int i = 0; i < 10000; i++)
-            {
-                double d = rand.NextDouble();
-                ds.AddDataPoint(new double[] { d }, new double[] { d });
-            }*/
+            DataSet.Regression ds = new DataSet.Regression(2, 1);
+            ds.AddDataPoint(new double[] { 0.2, 0.6 }, new double[] { 0.8 });
             ds.AssignDataPoints(1, 0, 0);
+
             Console.WriteLine("\nData set split in the ratio {0}, {1}, {2}.\n",
                 ds.TrainingSet.Length, ds.ValidationSet.Length, ds.TestSet.Length);
 
             // Create neural net
             FeedForwardNetwork ffn = new FeedForwardNetwork(2, 1);
-            ffn.AddHiddenLayer(4, new ActivationFunction.Sigmoid());
+            ffn.AddHiddenLayer(2, new ActivationFunction.Sigmoid());
             ffn.AddOutputLayer(new ActivationFunction.Sigmoid());
 
+            // Create weight matrices
+            Matrix w0 = new Matrix(2, 3);
+            w0[0, 0] = -0.3;
+            w0[1, 0] = 0.1;
+            w0[0, 1] = 0.4;
+            w0[1, 1] = -0.2;
+            w0[0, 2] = -0.6;
+            w0[1, 2] = 0.3;
+
+            Matrix w1 = new Matrix(1, 3);
+            w1[0, 0] = -0.2;
+            w1[0, 1] = 0.1;
+            w1[0, 2] = 0.2;
+
             // Create batch selector
-            DataPoint[] select(DataSet data) => ds.GetRandomTrainingSubset(4);
+            DataPoint[] select(DataSet data) => ds.GetRandomTrainingSubset(1);
 
             // Create backprop trainer
             BackpropagationTrainer t = new BackpropagationTrainer
             {
                 stochastic = false,
                 IndividualLearningRates = false,
-                LearningRate = 0.005,
-                initialiser = new Initialiser.Uniform(-0.2, 0.2, false),
+                LearningRate = 1,
+                initialiser = new Initialiser.CustomTest(new Matrix[] { w0, w1 }),
                 lossFunction = new LossFunction.SquaredError(),
                 batchSelector = new BackpropagationTrainer.BatchSelector(select),
-                evaluationFrequency = 2000
+                evaluationFrequency = 5
             };
 
-            t.terminationConditions.Add(new TerminationCondition.EpochLimit(50000));
+            t.terminationConditions.Add(new TerminationCondition.EpochLimit(0));
             t.Train(ffn, ds);
 
             // Troubleshoot trainer
@@ -76,25 +82,6 @@ namespace SharpNetDebug
             foreach (double[] arr in evals) Console.WriteLine("epoch={0}, training error={1}, " +
                 "validation error={2}", arr[0], arr[1], arr[2]);
             Console.WriteLine();
-
-            // Manual tests
-            for (int i = 0; i < 1; i++)
-            {
-                Matrix m = new Matrix(2, 1);
-                m[0, 0] = (rand.NextDouble() > 0.5) ? 1 : 0;
-                m[1, 0] = (rand.NextDouble() > 0.5) ? 1 : 0;
-                ffn.Input = m;
-                Console.WriteLine(ffn.Layers[0].Input.ToDetailedString());
-                Console.WriteLine(((FeedForwardLayer.Dense)ffn.Layers[0]).Weights.ToDetailedString());
-                Console.WriteLine(ffn.Layers[0].Output.ToDetailedString());
-                Console.WriteLine(ffn.Layers[1].Input.ToDetailedString());
-                Console.WriteLine(((FeedForwardLayer.Dense)ffn.Layers[1]).Weights.ToDetailedString());
-                Console.WriteLine(ffn.Layers[1].Output.ToDetailedString());
-            }
-
-            Console.WriteLine();
-
-            //Console.WriteLine("Backprop trainer ready?  {0}", t.IsReady());
 
             #endregion  // DEBUG_CODE
 
---
+++ SharpNetTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharpNetTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharpNetTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("54b2ccc7-63f0-4bc7-9f1f-e07f0d7027b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
