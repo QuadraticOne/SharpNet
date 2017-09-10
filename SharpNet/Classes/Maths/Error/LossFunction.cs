a6e0e0046119fe8b8e90655aa7299a90d0f7675e
aatack <alexanderjtatack@gmail.com>
2017-09-10 17:35:55 +0100

    Add squared error loss function

--- SharpNet/Classes/Maths/Error/LossFunction.cs
+++ SharpNet/Classes/Maths/Error/LossFunction.cs
@@ -11,6 +11,42 @@ namespace SharpNet.Classes.Maths.Error
     public class LossFunction
     {
 
+        /// <summary>
+        /// Loss is equal to half the magnitude of the difference between the output and target
+        /// vectors.
+        /// </summary>
+        public class SquaredError : ILossFunction
+        {
+
+            /// <summary>
+            /// Return the error between an output and a target.
+            /// </summary>
+            /// <param name="output"></param>
+            /// <param name="target"></param>
+            /// <returns></returns>
+            public double Error(Matrix output, Matrix target)
+            {
+                Matrix difference = target - output;
+                double error = 0;
+                for (int i = 0; i < difference.Rows; i++)
+                    error += difference[i, 0] * difference[i, 0];
+                return 0.5 * error;
+            }
+
+            /// <summary>
+            /// Return the derivative of the error with respect to the ith output.
+            /// </summary>
+            /// <param name="output"></param>
+            /// <param name="target"></param>
+            /// <param name="i"></param>
+            /// <returns></returns>
+            public double ErrorDerivative(Matrix output, Matrix target, int i)
+            {
+                return output[i, 0] - target[i, 0];
+            }
+
+        }
+
     }
 
 }
