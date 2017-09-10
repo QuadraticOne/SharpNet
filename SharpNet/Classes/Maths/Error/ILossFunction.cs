e85679ecc992e5ffe549dc676b50dc72fd13749a
aatack <alexanderjtatack@gmail.com>
2017-09-10 02:25:26 +0100

    Slight change to layout

---
+++ SharpNet/Classes/Maths/Error/ILossFunction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Maths.Error
+{
+
+    /// <summary>
+    /// Defines behaviour for loss functions.
+    /// </summary>
+    public interface ILossFunction
+    {
+
+        /// <summary>
+        /// Calculate the error between the output vector of a matrix and the training target.
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        double Error(Matrix output, Matrix target);
+
+        /// <summary>
+        /// Calculate the change in error between the given network output and target with respect
+        /// to the ith element.
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="target"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        double ErrorDerivative(Matrix output, Matrix target, int i);
+
+    }
+
+}
---
+++ SharpNet/Classes/Maths/Error/LossFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.Maths.Error
+{
+
+    /// <summary>
+    /// Contains some commonly used loss functions.
+    /// </summary>
+    public class LossFunction
+    {
+
+    }
+
+}
