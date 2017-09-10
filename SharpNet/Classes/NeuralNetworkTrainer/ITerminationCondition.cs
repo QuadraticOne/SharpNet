becf14cbb7e2655f01c170ab1f8ab8cd28348dee
aatack <alexanderjtatack@gmail.com>
2017-09-10 17:34:30 +0100

    Implement termination condition interface Implement common termination condition

---
+++ SharpNet/Classes/NeuralNetworkTrainer/ITerminationCondition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.NeuralNetworkTrainer
+{
+
+    /// <summary>
+    /// Defines the behaviour of classes which check to see whether a trainer should terminate its
+    /// training.
+    /// </summary>
+    public interface ITerminationCondition
+    {
+
+        /// <summary>
+        /// Return true if the trainer has finished.  Some termination conditions may require a
+        /// specific kind of trainer, in which case a cast will be necessary.
+        /// </summary>
+        /// <param name="trainer"></param>
+        /// <returns></returns>
+        bool HasFinished(ITrainer trainer);
+
+    }
+}
---
+++ SharpNet/Classes/NeuralNetworkTrainer/TerminationCondition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpNet.Classes.NeuralNetworkTrainer
+{
+
+    /// <summary>
+    /// Defines some commonly used training termination conditions.
+    /// </summary>
+    public class TerminationCondition
+    {
+
+        /// <summary>
+        /// Stops training after a certain number of epochs have occurred.
+        /// </summary>
+        public class EpochLimit : ITerminationCondition
+        {
+
+            private int limit;
+
+            /// <summary>
+            /// Create a new epoch limit termination condition.
+            /// </summary>
+            /// <param name="limit"></param>
+            public EpochLimit(int limit)
+            {
+                this.limit = limit;
+            }
+
+            public bool HasFinished(ITrainer trainer)
+            {
+                return trainer.GetEpoch() > limit;
+            }
+
+        }
+
+    }
+
+}
