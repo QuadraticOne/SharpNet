8b8125e3203e8853e13a9d9818a11b15d9019066
aatack <alexanderjtatack@gmail.com>
2017-09-11 00:29:57 +0100

    Successfully test XOR example

--- SharpNetDebug/Program.cs
+++ SharpNetDebug/Program.cs
@@ -31,7 +31,11 @@ namespace SharpNetDebug
 
             // Create data set
             DataSet.Regression ds = new DataSet.Regression(2, 1);
-            ds.AddDataPoint(new double[] { 0.2, 0.6 }, new double[] { 0.8 });
+            ds.AddDataPoint(new double[] { 0, 0 }, new double[] { 0 });
+            ds.AddDataPoint(new double[] { 0, 1 }, new double[] { 1 });
+            ds.AddDataPoint(new double[] { 1, 0 }, new double[] { 1 });
+            ds.AddDataPoint(new double[] { 1, 1 }, new double[] { 0 });
+            //ds.AddDataPoint(new double[] { 0.2, 0.6 }, new double[] { 0.8 });
             ds.AssignDataPoints(1, 0, 0);
 
             Console.WriteLine("\nData set split in the ratio {0}, {1}, {2}.\n",
@@ -39,9 +43,10 @@ namespace SharpNetDebug
 
             // Create neural net
             FeedForwardNetwork ffn = new FeedForwardNetwork(2, 1);
-            ffn.AddHiddenLayer(2, new ActivationFunction.Sigmoid());
+            ffn.AddHiddenLayer(5, new ActivationFunction.Sigmoid());
             ffn.AddOutputLayer(new ActivationFunction.Sigmoid());
 
+            /*
             // Create weight matrices
             Matrix w0 = new Matrix(2, 3);
             w0[0, 0] = -0.3;
@@ -55,23 +60,24 @@ namespace SharpNetDebug
             w1[0, 0] = -0.2;
             w1[0, 1] = 0.1;
             w1[0, 2] = 0.2;
+            */
 
             // Create batch selector
-            DataPoint[] select(DataSet data) => ds.GetRandomTrainingSubset(1);
+            DataPoint[] select(DataSet data) => ds.TrainingSet;
 
             // Create backprop trainer
             BackpropagationTrainer t = new BackpropagationTrainer
             {
                 stochastic = false,
                 IndividualLearningRates = false,
-                LearningRate = 1,
-                initialiser = new Initialiser.CustomTest(new Matrix[] { w0, w1 }),
+                LearningRate = 0.15,
+                initialiser = new Initialiser.Uniform(-0.2, 0.2, false),
                 lossFunction = new LossFunction.SquaredError(),
                 batchSelector = new BackpropagationTrainer.BatchSelector(select),
-                evaluationFrequency = 5
+                evaluationFrequency = 10000
             };
 
-            t.terminationConditions.Add(new TerminationCondition.EpochLimit(0));
+            t.terminationConditions.Add(new TerminationCondition.EpochLimit(100000));
             t.Train(ffn, ds);
 
             // Troubleshoot trainer
@@ -82,6 +88,21 @@ namespace SharpNetDebug
             foreach (double[] arr in evals) Console.WriteLine("epoch={0}, training error={1}, " +
                 "validation error={2}", arr[0], arr[1], arr[2]);
             Console.WriteLine();
+
+            // Testing
+            Random rand = new Random();
+            int tests = 15;
+            int correct = 0;
+            for (int i = 0; i < tests; i++)
+            {
+                DataPoint dp = ds.GetRandomTrainingExample();
+                double o = ffn.GetOutput(Matrix.ToColumnMatrix(dp.input))[0, 0];
+                double oup = dp.output[0];
+                Console.WriteLine(o + " " + oup);
+                if (((o > 0.5) && (oup == 1)) || ((o < 0.5) && (oup == 0))) correct++;
+            }
+
+            Console.WriteLine("Tests correct: {0}/{1}", correct, tests);
 
             #endregion  // DEBUG_CODE
 
